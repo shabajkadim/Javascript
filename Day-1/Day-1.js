8fe2966624514a4742b9be76f269b30337780201
Shahabaj kadim <shahabajkadim7@gmail.com>
2023-10-26 14:27:01 +0530

    updates

---
+++ Day-1/Day-1.js
@@ -0,0 +1,72 @@
+console.log("shahabaj")
+console.log("shahabaj")
+
+console.log('javascrip' )
+console.log('javascrip' )
+
+console.log(`Html-css`)
+console.log(`Html-css`)
+
+console.log('html + react')
+console.log(`5`+ `8`)
+
+console.log(5 + 3)
+console.log(typeof(2+1))
+console.log(5 + 2)
+console.log(typeof( 5+3))
+console.log(3 + 3)
+console.log(typeof(2+6) )
+
+console.log("shahabaj")
+console.log(typeof("Shahabaj"))
+
+console.log('javascrip' )
+console.log(typeof('javascript')) 
+
+console.log(`Html-css`)
+console.log(typeof( `html-css`))
+
+console.log('html + react')
+console.log(`5`+ `8`)
+
+
+console.log(typeof( 5+3))
+console.log(typeof(2+6) )
+console.log(typeof(2+1))
+console.log(typeof(2+6) )
+
+console.log("shahabaj")
+console.log("shahabaj")
+
+console.log('javascrip' )
+console.log('javascrip' )
+
+console.log(`Html-css`)
+console.log(`Html-css`)
+
+console.log('html + react')
+console.log(`5`+ `8`)
+
+console.log(5 + 3)
+console.log(typeof(2+6) )
+console.log(5 + 2)
+console.log(typeof(2+6) )
+console.log(3 + 3)
+console.log(typeof(2+6) )
+
+console.log("shahabaj")
+console.log(typeof("Shahabaj"))
+
+console.log('javascrip' )
+console.log(typeof('javascript')) 
+
+console.log(`Html-css`)
+console.log(typeof( `html-css`))
+
+console.log('html + react')
+console.log(`5`+ `8`)
+
+
+console.log(typeof( 5+3))
+console.log(typeof(2+1))
+console.log(typeof(2+6) )
