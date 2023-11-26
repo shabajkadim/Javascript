01f79b41db3f0b2c15d838209d1b0fa3515d4948
Shahabaj kadim <shahabajkadim7@gmail.com>
2023-11-26 13:31:53 +0530

    updates

---
+++ Day-12/Day-12.js
@@ -0,0 +1,94 @@
+    //var Re-assign
+var name ="Shahabaj";
+console.log(name)
+name = "Amit";
+console.log(name)
+name = 18;
+console.log(name)
+
+var myname="shahabaj";
+console.log(myname)
+myname="amit"
+console.log(myname)
+myname="dhanaraj"
+console.log(myname)
+
+var myname="shahabaj";
+console.log(myname)
+myname="amit"
+console.log(myname)
+myname="dhanaraj"
+console.log(myname)
+
+
+    //var  Re-declare
+var myname="amit";
+myname= "shahabaj";
+console.log(myname)
+var myname="shahabaj"
+
+var number=20;
+number = 18;
+console.log(number)
+var number=18;
+
+var myname="shahabaj";
+myname="amit"
+console.log(myname)
+var myname="dhanaraj"
+
+
+var myage=22;
+myage=20;
+console.log(myage)
+var myage=20;
+
+//let Re-assign
+let Myname ="virat";
+console.log(Myname)
+Myname ="Rohit"
+console.log(Myname)
+
+//let Re-declare
+
+let MyAge=23;
+MyAge=20;
+console.log(MyAge)
+ let MyAge=20;
+
+// const Re-assign
+const myage=20;
+console.log(myage)
+myage =21;
+console.log(myage)
+
+// const Red-declare
+const NAME="shahabaj";
+console.log(NAME)
+NAME="Amit"
+console.log(NAME)
+
+// const Re-declare
+const number=18;
+number=20
+console.log(number)
+const number=20;
+
+
+// Hoisting-only for var varaible and traditional function
+
+myage=25;
+console.log(myage)
+var myage;   
+
+myname="shahabaj";
+console.log(myname)
+var myname;
+
+myname ="devesh";
+console.log(myname)
+let myname;
+
+myage=22;
+console.log(myage)
+const myage; 
